7b10885ec51d257f5f4174003f1ebec57c049a54
Michael Aragon <76224826+michaeljaragon@users.noreply.github.com>
2021-02-23 13:56:26 -0700

    aksjfd

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name) 
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Need at least 5 students.");
+            }
+            else
+            {
+                int increment = (int)Math.Ceiling(Students.Count * 0.2);
+                var grades = Students.OrderBy(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+
+                if (averageGrade >= grades[increment - 1])
+                    return 'A';
+                if (averageGrade >= grades[increment * 2 - 1])
+                    return 'B';
+                if (averageGrade >= grades[increment * 3 - 1])
+                    return 'C';
+                if (averageGrade >= grades[increment * 4 - 1])
+                    return 'D';
+                return 'F';
+            }
+        }
+    }
+}
